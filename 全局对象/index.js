d584cac4276854378b52ed4ef78a90a50edddd43
tyq <2903474904@qq.com>
2021-05-10 19:23:04 +0800

    全局对象

--- 全局对象/index.js
+++ 全局对象/index.js
@@ -1,1 +1,34 @@
-console.log(global);
+// console.log(global);
+// const timer = setImmediate(() => {
+//     console.log('ada')
+// }, 0);
+// console.log(timer);
+
+// 绝对路径
+// console.log(__dirname);
+// console.log(__filename);
+
+// 类型化数组
+// const buffer = Buffer.from('abcdefg', 'utf-8');
+// console.log(buffer);
+
+// 命令行所在路径
+// console.log(process.cwd());
+
+// setTimeout(() => {
+//     console.log('abc');
+// }, 0)
+// 强制退出进程
+// process.exit();
+
+// 命令行参数
+// console.log(process.argv);
+
+// 操作系统
+// console.log(process.platform);
+
+// 销毁某个进程，给定进程的ID
+// process.kill();
+
+// 获得环境变量
+console.log(process.env);
